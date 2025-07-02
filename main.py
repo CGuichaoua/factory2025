4f05ce5521cc86037610347d01aa820a81ca84cf
Corentin Guichaoua <corentin@jumbomana.fr>
2025-07-02 16:53:22 +0200

    Minor changes

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import os
-from sys import api_version
 from openai import AzureOpenAI
 
 def main():
     azure_client=create_azure_client()
     response = azure_client.chat.completions.create(
-        model=os.environ['MODEL'],  # this is the Azure "deployment name"
+        model=os.environ['MODEL'],
         messages=[
             {"role": "system", "content": "You are a helpful assistant, here to help us brainstorm in Haguenau for the GenCity hackathon."},
             {"role": "user", "content": "Give me a cool idea to use generative AI in cities. Include some simple steps to start prototyping. Keep it simple so that it is possible to achieve a minimal version in 3 hours."}
